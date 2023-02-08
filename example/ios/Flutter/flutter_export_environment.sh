a5e6aa0e8127b64e63bcf99bc482e6489b0542dd
Elliot Sayes <elliot@sayes.cloud>
2023-02-08 12:51:42 +0800

    Rerun codegen (working on web)

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/elliotsayes/Library/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/elliotsayes/Code/Flutter/flutter_rust_bridge_cipher_test/example"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
